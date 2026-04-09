5a574f5d9332f145e05185522b2a697c3f0fc33d
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-09 18:22:18 +0200

    refactor: put some structure and add codewars

---
+++ codewars/ReversedStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine(Solution("Reverse"));
+  }
+  public static string Solution(string str) 
+  {
+    string result = "";
+    
+    for (int i=str.Length-1; i >=0; i--)
+    {   
+       result+=str[i];
+    }
+     
+    return result;
+  }
+}
+
