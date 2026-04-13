5b8047e702352f48e7165b0ec6f4fa1d08e24f4f
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-13 15:19:48 +0200

    refactor: rename methods and improve output formatting in TodoApp

--- projects/TodoApp/TodoApp.cs
+++ projects/TodoApp/TodoApp.cs
@@ -5,46 +5,52 @@ namespace TodoProject
         private static int todoId = 0;
         private static readonly List<Todo> todolist = [];
 
-        public static void AddNewTask(string title, string desc)
+        public static void AddNewTodo(string title, string desc)
         {
-            Console.WriteLine("");
             Todo newTask = new(title, desc, id: ++todoId);
             todolist.Add(newTask);
-            Console.WriteLine($"New Task: \"{newTask.Title}\" was added successfully!");
+            Console.WriteLine($"\nNew Task: \"{newTask.Title}\" was added successfully!");
         }
         public static void DisplayTodoList()
         {
-            Console.WriteLine("List of Todos:");
+            if (todolist.Count == 0)
+            {
+                Console.WriteLine("\nNo Todos!");
+                return;
+            }
+            Console.WriteLine("\nList of Todos:");
             foreach (Todo task in todolist)
             {
                 Console.WriteLine(task);
             }
         }
-        public static void ModifyStatus(int id)
+        public static void ToggleTodoStatus(int id)
         {
-            var task = FindOneTask(id);
+            var task = FindOneTodo(id);
             task.Status = !task.Status;
-
+            Console.WriteLine($"Status of Task No{id}, changed to {task.Status}");
         }
         public static void UpdateTodo(int id, string? title, string? description)
         {
-            Todo task = FindOneTask(id);
+            Todo task = FindOneTodo(id);
             task.Title = title ?? task.Title;
             task.Description = description ?? task.Description;
+            Console.WriteLine(task.ToString());
+            Console.WriteLine($"Task No{id} updated sucessfully!");
         }
         public static void RemoveTodo(int id)
         {
             var taskIndex = todolist.FindIndex(task => task.Id == id);
             todolist.RemoveAt(taskIndex);
-            Console.WriteLine($"Task No{id} Removed Successfully!");
+            Console.WriteLine($"\nTask No{id} removed successfully!");
         }
         public static void ClearTodos()
         {
             todolist.Clear();
-            Console.WriteLine("Todo list cleared!");
+            Console.WriteLine("\nTodo list cleared!");
         }
 
-        static private Todo FindOneTask(int id)
+        static private Todo FindOneTodo(int id)
         {
             var task = todolist.First(task => task.Id == id) ?? throw new Exception("Task not found!");
             return task;
@@ -52,7 +58,7 @@ namespace TodoProject
 
         public static void RunApp()
         {
-            Console.WriteLine("====================== Welcome to the Todo App ======================");
+            Console.WriteLine("\n====================== Welcome to the Todo App ======================");
             Console.WriteLine("Available Options: ");
             Console.WriteLine("1. Add a new task");
             Console.WriteLine("2. Display the todo list");
