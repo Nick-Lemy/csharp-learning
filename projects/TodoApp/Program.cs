01e1abe04df05ac028c1378f4a7e6d51f46d5e50
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-13 15:19:43 +0200

    feat: restructure Main method in TodoApp for improved task management flow

--- projects/TodoApp/Program.cs
+++ projects/TodoApp/Program.cs
@@ -1,10 +1,58 @@
 ﻿using TodoProject;
 
-TodoApp.RunApp();
-if (!int.TryParse(Console.ReadLine(), out int option))
+internal class Program
 {
-    Console.WriteLine("Invalid input! Please enter a number corresponding to the options.");
-    TodoApp.RunApp();
-}
+    private static void Main(string[] args)
+    {
+        TodoApp.RunApp();
+        if (!int.TryParse(Console.ReadLine(), out int option))
+        {
+            Console.WriteLine("Invalid input! Please enter a number corresponding to the options.");
+            Main([]);
+        }
 
-Console.WriteLine(option);
+        switch (option)
+        {
+            case 1:
+                Console.Write("Enter new task's title: ");
+                string? title = Console.ReadLine();
+
+                Console.Write("Enter new task's description: ");
+                string? desc = Console.ReadLine();
+
+                if (title == null || desc == null)
+                {
+                    Console.WriteLine("Invalid input, please enter a valid title or description");
+                    Main([]);
+                    break;
+                }
+                TodoApp.AddNewTodo(title, desc);
+                Main([]);
+                break;
+            case 2:
+                TodoApp.DisplayTodoList();
+                Main([]);
+                break;
+            case 3:
+                Console.Write("Enter id of the Task to update: ");
+                if (int.TryParse(Console.ReadLine(), out int id))
+                {
+                    Console.WriteLine("Invalid input");
+                    Main([]);
+                    break;
+                }
+
+                Console.WriteLine("Enter updated title: ");
+                string? updatedTitle = Console.ReadLine();
+
+                Console.WriteLine("Enter updated description: ");
+                string? updatedDescription = Console.ReadLine();
+                TodoApp.UpdateTodo(id, description: updatedDescription?.Length <= 1 ? null : updatedDescription, title: updatedTitle?.Length <= 1 ? null : updatedTitle);
+                break;
+            case 7:
+                break;
+            default:
+                break;
+        }
+    }
+}
