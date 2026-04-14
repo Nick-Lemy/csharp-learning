f498a476058a4d91ee3355406755413bd346c1df
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-14 18:44:32 +0200

    feat: add initial implementation of NumbersToLetters project

---
+++ codewars/NumbersToLetters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Switcher(["24", "12", "23", "22", "4", "26", "9", "8"]));
+    }
+    public static string Switcher(string[] x)
+    {
+        List<char> resultChars = [];
+        List<char> alphabet = [.. " ?!abcdefghijklmnopqrstuvwxyz-".Reverse()];
+        foreach (string str in x) resultChars.Add(alphabet[Convert.ToInt32(str)]);
+        return string.Join("", resultChars);
+    }
+
+}
+
