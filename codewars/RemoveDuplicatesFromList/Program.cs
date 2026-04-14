288d346ff0c1c10a483d3bee295283852847feab
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-14 20:13:07 +0200

    feat: add initial implementation of RemoveDuplicatesFromList project with main functionality

---
+++ codewars/RemoveDuplicatesFromList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9];
+            Console.WriteLine(string.Join(", ", distinct(a)));
+        }
+        public static int[] distinct(int[] a)
+        {
+            return new HashSet<int>(a).ToArray();
+        }
+
+    }
+}
