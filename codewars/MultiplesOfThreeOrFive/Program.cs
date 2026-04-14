08af20f3eba38708524ff8c956bd6a611a2801b2
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-14 19:31:32 +0200

    feat: add initial implementation of MultiplesOfThreeOrFive project with main functionality

---
+++ codewars/MultiplesOfThreeOrFive/Program.cs
@@ -0,0 +1,13 @@
+﻿public static class Kata
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Solution(10));
+    }
+    public static int Solution(int value)
+    {
+        int count = 0;
+        for (int i = 2; i < value; i++) if (i % 3 == 0 || i % 5 == 0) count += i;
+        return count;
+    }
+}
