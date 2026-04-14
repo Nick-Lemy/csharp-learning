ef30889622aa54ffdb46c6060a0d5813498f3db2
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-14 19:39:33 +0200

    feat: add initial implementation of Factorial project with main functionality

---
+++ codewars/Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Kata
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Factorial(5));
+    }
+    public static int Factorial(int n)
+    {
+        int result = 1;
+
+        if (n < 0 || n > 12) throw new ArgumentOutOfRangeException();
+        for (int i = 1; i <= n; i++) result *= i;
+
+        return result;
+    }
+}
