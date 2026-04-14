4464b0495ecbc95828f232d95d599d68c302ef6a
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-14 19:35:13 +0200

    feat: add initial implementation of FindTheSmallestIntegerInTheArray project with main functionality

---
+++ codewars/FindTheSmallestIntegerInTheArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+public class Kata
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = [5, 3, 2, -74, 1, 4];
+        Console.WriteLine(FindSmallestInt(numbers));
+    }
+    public static int FindSmallestInt(int[] args)
+    {
+        return args.Min();
+    }
+}
