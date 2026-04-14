34f1bb564f1ab14f9b1eacd01eae854a1c739ac1
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-14 19:25:33 +0200

    feat: add initial implementation of Ship class and project file

---
+++ codewars/ObjectOrientedOPiracy/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Ship
+{
+    public int Draft;
+    public int Crew;
+
+    public Ship(int draft, int crew)
+    {
+        Draft = draft;
+        Crew = crew;
+    }
+
+    public bool IsWorthIt()
+    {
+        return Draft - (1.5 * Crew) > 20;
+    }
+
+}
