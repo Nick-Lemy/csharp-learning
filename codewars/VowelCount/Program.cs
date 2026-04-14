c1c1218a84fedc3046db73b4ccc2d89e2e362185
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-14 19:52:23 +0200

    feat: add initial implementation of VowelCount project with main functionality

---
+++ codewars/VowelCount/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(GetVowelCount("abracadabra"));
+    }
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+        foreach (char letter in str) vowelCount += "aeiou".Contains(letter) ? 1 : 0;
+
+        return vowelCount;
+    }
+}
